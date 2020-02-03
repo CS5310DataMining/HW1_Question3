6891b292e74a031b2e0a23d49829c9d4be2710b7
rbeiza <60162953+rbeiza@users.noreply.github.com>
2020-02-02 22:16:38 -0600

    Update HW1_Q3_Team6.R

--- HW1_Q3_Team6.R
+++ HW1_Q3_Team6.R
@@ -1,9 +1,9 @@
 # HW_1 Questio_3
 #Team: 6
 #Team Members:
-  #Melvin Zaldivar - Members contribution: 33.33%
-  #Rahim Abdulmalik - Members contribution: 33.33%
-  #Raul Beiza - Members contribution: 33.33%
+#Melvin Zaldivar - Members contribution: 33.33%
+#Rahim Abdulmalik - Members contribution: 33.33%
+#Raul Beiza - Members contribution: 33.33%
 
 # Due Date: February 2, 2020
 
@@ -20,12 +20,18 @@ iris_training <- read.csv("iris_training.csv", stringsAsFactors = FALSE)
 # Import the iris test data set
 iris_test <- read.csv("iris_test.csv", stringsAsFactors = FALSE)
 
+# Switch Species from character to factor
+iris_training$Species <- factor(iris_training$Species)
+
 data.frame(iris_training)
 data.frame(iris_test)
 
 # Remove the ID feature
 iris_training <-iris_training[-1]
 iris_test <- iris_test[-1]
+
+# Adding Specites column to test data
+iris_test$Species <- factor(0)
 
 # Obtain outcome of the Species and create table
 table(iris_training$Species)
@@ -37,11 +43,43 @@ round(prop.table(table(iris_training$Species))*100, digits = 1)
 normalize <- function(x) {
   return ((x - min(x))/(max(x)-min(x)))
 }
-  
+
 # Apply the normalize function to columns 2 through 31
-iris_training_n <-as.data.frame(lapply(iris_training[1:4], normalize)) 
+iris_training_n <-as.data.frame(lapply(iris_training[1:4], normalize))
+iris_test_n <-as.data.frame(lapply(iris_test[1:4], normalize)) 
 
 # Confrim the transformation was applied correctly
 summary(iris_training_n)
 
 # Creating labels
+iris_train_labels <- iris_training[1:105,5]
+print(iris_train_labels)
+
+iris_test_labels <- iris_test[106:150,5]
+print(iris_test_labels)
+
+#================================================
+# Step-2: Train a model on the data
+#================================================
+
+# Install the "class" package for k-NN
+install.packages("class")
+
+# Load the "class package for k-NN
+library(class)
+
+iris_test_pred <- knn(train=iris_training_n, test=iris_test_n, cl=iris_train_labels, k=10)
+
+#================================================
+# Step-3: Evaluate the model performance
+#================================================
+
+# Installing gmodels to perform evaluation on train and test data
+install.packages("gmodels")
+
+# loading gmodels 
+library(gmodels)
+
+# Evaluting how well the predicted classes match the
+# actual values 
+
